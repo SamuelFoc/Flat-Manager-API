f01d8090c92dbc35d707bbfd7da16b178cc31652
SamuelFoc <samo.sipikal@gmail.com>
2022-09-25 20:12:07 +0200

    Cancel SQL logging from sequelize

--- app/util/database.js
+++ app/util/database.js
@@ -3,6 +3,7 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize("sqlite-db", "admin-samuel", "admin-root", {
   dialect: "sqlite",
   storage: "./app/DB/database.sqlite",
+  logging: false,
 });
 
 module.exports = sequelize;
