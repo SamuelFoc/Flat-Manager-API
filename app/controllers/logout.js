d5381b6c9583b3b1d1e4e2a4336b31d9cc3f6d61
SamuelFoc <samo.sipikal@gmail.com>
2022-09-27 09:15:34 +0200

    Edit logout TYPO

--- app/controllers/logout.js
+++ app/controllers/logout.js
@@ -3,34 +3,35 @@ const jwt = require("jsonwebtoken");
 
 const handleLogout = async (req, res) => {
   // ! On client side also delete the access_token !!
-
   // * Find refresh token in cookies
   const cookies = req.cookies;
+
   if (!cookies?.jwt) return res.sendStatus(204);
   const refreshToken = cookies.jwt;
 
   // * Find user with certain refresh token in DB
-  const user = await User.findOne({
-    where: { refresh_token: refreshToken },
-  });
-  // * - If there is no user with this refresh token clear cookies
-  if (!user) {
-    res.clearCookie("jwt", {
-      httpOnly: true,
-      sameSite: "None",
-      secure: true,
-    });
-    return res.sendStatus(204);
-  }
-
-  // * Delete refresh token from DB and clear cookies
-  user
-    .update({ refresh_token: null })
+  User.findOne({
+    where: { refreshToken: refreshToken },
+  })
+    .then((user) => {
+      // * Delete refresh token from DB
+      return user.update({ refreshToken: null });
+    })
+    // * Clear cookies
     .then(() => {
       res.clearCookie("jwt", { httpOnly: true });
       res.sendStatus(204);
     })
-    .catch((err) => res.sendStatus(500));
+    .catch((err) => {
+      // * - If there is no user with this refresh token clear cookies
+      console.error(err);
+      res.clearCookie("jwt", {
+        httpOnly: true,
+        sameSite: "None",
+        secure: true,
+      });
+      return res.sendStatus(204);
+    });
 };
 
 module.exports = {
