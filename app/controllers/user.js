926d8baa2903c3bc7d10c085870de35ce05a6e35
SamuelFoc <samo.sipikal@gmail.com>
2022-09-27 09:14:01 +0200

    Remove create one controller from User and place it into Admin

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -52,56 +52,6 @@ exports.getOne = async (req, res) => {
     });
 };
 
-exports.createOne = async (req, res) => {
-  const { user, pwd, email, contact, age, work, isAdmin, room } = req.body;
-  let count;
-
-  // * Check for required parameters
-  if (!user || !pwd || !email)
-    return res
-      .status(400)
-      .json({ message: "Username, email and passsword required!" });
-
-  // * Check if user already exists
-  const userExists = await User.findOne({
-    where: { username: user },
-  });
-  if (userExists)
-    return res.status(409).send("User with that name already exists!");
-
-  // * Hash the password
-  const hashedPwd = await bcrypt.hash(pwd, 10);
-
-  // * Create user model
-  const USER_MODEL = {
-    username: user,
-    email: email,
-    password: hashedPwd,
-    contact: contact ? contact : "Unknown",
-    age: age ? age : null,
-    work: work ? work : "Student",
-    isAdmin: isAdmin ? isAdmin : false,
-  };
-
-  // * Create user
-  User.create(USER_MODEL)
-    .then(async (user) => {
-      if (room !== undefined) {
-        const foundRoom = await Room.findOne({ where: { name: room } });
-        await foundRoom.addUser(user);
-      }
-      console.log("Room created");
-      const role = await user.createRole({ roleName: "User" });
-      return user.addRole(role);
-    })
-    .then(() => {
-      res.status(200).send("User created successfully!");
-    })
-    .catch((err) => {
-      res.status(400).send(err);
-    });
-};
-
 exports.deleteOne = async (req, res) => {
   let count;
   sequelize
--- app/routes/user.js
+++ app/routes/user.js
@@ -4,7 +4,6 @@ const router = require("express").Router();
 router
   .get("/", controller.getAll)
   .get("/:email", controller.getOne)
-  .post("/", controller.createOne)
   .put("/:email", controller.updateOne)
   .delete("/:email", controller.deleteOne);
 
