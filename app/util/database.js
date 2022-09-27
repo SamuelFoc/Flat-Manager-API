512aae69ce0f1f0c524913b5e0a50dc1d9596220
SamuelFoc <samo.sipikal@gmail.com>
2022-09-27 09:15:53 +0200

    Add testing database

--- app/util/database.js
+++ app/util/database.js
@@ -1,8 +1,11 @@
 const Sequelize = require("sequelize");
+require("dotenv").config();
 
 const sequelize = new Sequelize("sqlite-db", "admin-samuel", "admin-root", {
   dialect: "sqlite",
-  storage: "./app/DB/database.sqlite",
+  storage: process.env.DEV
+    ? "./app/DB/test-database.sqlite"
+    : "./app/DB/database.sqlite",
   logging: false,
 });
 
