b4de7cec20f21e9b03d3445b73ccc29892decb34
SamuelFoc <samo.sipikal@gmail.com>
2022-09-27 16:43:53 +0200

    Remove logs

--- app/middleware/verifyToken.js
+++ app/middleware/verifyToken.js
@@ -9,7 +9,6 @@ const verifyJWT = (req, res, next) => {
   const token = authHeader.split(" ")[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) return res.status(403).json("Invalid token!");
-    console.log(decoded);
     req.user = decoded.UserInfo.username;
     req.roles = decoded.UserInfo.roles;
     next();
